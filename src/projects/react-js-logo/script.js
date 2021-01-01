31e5f4e593998bbdbf8c8ebdf3cd80a76b4f192c
Leonid Velichko <vlsrnd@gmail.com>
2021-01-01 17:28:25 +0300

    refract react-js-logo. fix bugs in parametrics dependecies. add shadow and function for move shadow

--- src/projects/react-js-logo/script.js
+++ src/projects/react-js-logo/script.js
@@ -13,38 +13,48 @@ context.fillStyle = 'hsla(0, 0%, 0%, 1)';
 //main settings
 const ellipsW = canvas.width / 4;
 const ellipsH = 0.4 * ellipsW;
-const hMax = 0.3 * ellipsW;
-const r = 1;
+const h = 0.2 * ellipsW;
+const r = 10;
+const mainColor = `hsla(193, 95%, 68%, 1)`;
 
-let t = 0;
-let h = 0;
-let hUp = true;
+const maxShadowOffset = 10;
+context.fillStyle = mainColor;
+context.shadowColor = mainColor;
+context.shadowBlur = 5;
+context.shadowOffsetX = 0;
+context.shadowOffsetY = 0;
+
+const moveShadow = (e) => {
+  const deltaX = Math.floor(e.clientX / canvas.width * (2 * maxShadowOffset + 1)) -
+    maxShadowOffset;
+  const deltaY = Math.floor(e.clientY / canvas.height * (2 * maxShadowOffset + 1)) -
+    maxShadowOffset;
+  context.shadowOffsetX = -deltaX;
+  context.shadowOffsetY = -deltaY;
+};
+document.addEventListener('mousemove', moveShadow);
 
 const draw = () => {
-  context.fillStyle = '#222';
-  for (let i = 0; i < 6 * Math.PI; i += 0.05) {
-    context.fillStyle = `hsla(${Math.sin(Math.random()) * 50 +200}, 100%, 50%, 1)`;
-    //center
-    const xCenter = h * Math.sin(i) + canvas.width / 2;
-    const yCenter = h * Math.cos(i) + canvas.height / 2;
-    h = hUp ? h + 0.05 : h - 0.05;
-    hUp = h > hMax ? false : h < 0 ? true : hUp;
+  context.beginPath();
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.arc(canvas.width / 2, canvas.height / 2, h, 0, Math.PI * 2);
+  context.fill();
+  context.closePath();
+  for (let i = 0; i < 2 * Math.PI; i += 0.05) {
     context.beginPath();
-    context.arc(xCenter, yCenter, r, 0, Math.PI * 2);
-    context.fill();
+    context.fillStyle = `hsla(${Math.random() * 40 + 193}, 95%, 68%, 1)`;
     //oblique
-    const xEl = 1.8 * ellipsH * Math.sin(i) + canvas.width / 2;
-    const yEl0 = ellipsW * Math.cos(i + Math.PI / 4) + canvas.height / 2;
-    const yEl1 = ellipsW * Math.cos(i - Math.PI / 4) + canvas.height / 2;
-    context.beginPath();
-    context.arc(xEl, yEl0, r, 0, Math.PI * 2);
-    context.arc(xEl, yEl1, r, 0, Math.PI * 2);
+    const x0 = ellipsW * Math.sin(i);
+    const y0 = ellipsH * Math.cos(i);
+    const x1 = x0 * Math.cos(Math.PI / 3) - y0 * Math.sin(Math.PI / 3) + canvas.width / 2;
+    const y1 = x0 * Math.sin(Math.PI / 3) + y0 * Math.cos(Math.PI / 3) + canvas.height / 2;
+    const x2 = x0 * Math.cos(2 * Math.PI / 3) - y0 * Math.sin(2 * Math.PI / 3) + canvas.width / 2;
+    const y2 = x0 * Math.sin(2 * Math.PI / 3) + y0 * Math.cos(2 * Math.PI / 3) + canvas.height / 2;
+    context.arc(x1, y1, r, 0, Math.PI * 2);
+    context.arc(x2, y2, r, 0, Math.PI * 2);
     context.fill();
-    //horizontal
-    const xEl2 = ellipsW * Math.sin(i) + canvas.width / 2;
-    const yEl2 = ellipsH * Math.cos(i) + canvas.height / 2;
     context.beginPath();
-    context.arc(xEl2, yEl2, r, 0, Math.PI * 2);
+    context.arc(x0 + canvas.width / 2, y0 + canvas.height / 2, r, 0, Math.PI * 2)
     context.fill();
   }
 };
