31ec4dff24fe5dcb30e8a2dd57b2e1c3e38b537b
Leonid Velichko <vlsrnd@gmail.com>
2021-01-01 21:37:08 +0300

    fix some bugs, add neon shadow in react-logo, remove shadow moving

--- src/projects/react-js-logo/script.js
+++ src/projects/react-js-logo/script.js
@@ -1,48 +1,35 @@
-let canvas = document.querySelector('#canvasId');
-let context = canvas.getContext("2d");
+let canvas = document.querySelector('#canvasId'),
+  ctx = canvas.getContext("2d"),
+  ellipsW, ellipsH, h;
+const ellipseSize = 4,
+  r = 10,
+  mainColor = `hsla(193, 95%, 68%, 1)`;
 
 const resize = () => {
   canvas.setAttribute('width', document.documentElement.clientWidth);
   canvas.setAttribute('height', document.documentElement.clientHeight);
+  ellipsW = Math.min(canvas.width, canvas.height) / ellipseSize;
+  ellipsH = 0.4 * ellipsW;
+  h = 0.2 * ellipsW;
 };
 
 resize();
 window.addEventListener('resize', resize);
-context.fillStyle = 'hsla(0, 0%, 0%, 1)';
+ctx.fillStyle = 'hsla(0, 0%, 0%, 1)';
 
-//main settings
-const ellipsW = canvas.width / 4;
-const ellipsH = 0.4 * ellipsW;
-const h = 0.2 * ellipsW;
-const r = 10;
-const mainColor = `hsla(193, 95%, 68%, 1)`;
-
-const maxShadowOffset = 10;
-context.fillStyle = mainColor;
-context.shadowColor = mainColor;
-context.shadowBlur = 5;
-context.shadowOffsetX = 0;
-context.shadowOffsetY = 0;
-
-const moveShadow = (e) => {
-  const deltaX = Math.floor(e.clientX / canvas.width * (2 * maxShadowOffset + 1)) -
-    maxShadowOffset;
-  const deltaY = Math.floor(e.clientY / canvas.height * (2 * maxShadowOffset + 1)) -
-    maxShadowOffset;
-  context.shadowOffsetX = -deltaX;
-  context.shadowOffsetY = -deltaY;
-};
-document.addEventListener('mousemove', moveShadow);
+ctx.fillStyle = mainColor;
+ctx.shadowColor = mainColor;
+ctx.shadowBlur = 15;
 
 const draw = () => {
-  context.beginPath();
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.arc(canvas.width / 2, canvas.height / 2, h, 0, Math.PI * 2);
-  context.fill();
-  context.closePath();
-  for (let i = 0; i < 2 * Math.PI; i += 0.05) {
-    context.beginPath();
-    context.fillStyle = `hsla(${Math.random() * 40 + 193}, 95%, 68%, 1)`;
+  ctx.beginPath();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.arc(canvas.width / 2, canvas.height / 2, h, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.closePath();
+  for (let i = 0; i < 2 * Math.PI; i += 0.15) {
+    ctx.beginPath();
+    ctx.fillStyle = `hsla(${Math.random() * 40 + 193}, 95%, 68%, 1)`;
     //oblique
     const x0 = ellipsW * Math.sin(i);
     const y0 = ellipsH * Math.cos(i);
@@ -50,12 +37,12 @@ const draw = () => {
     const y1 = x0 * Math.sin(Math.PI / 3) + y0 * Math.cos(Math.PI / 3) + canvas.height / 2;
     const x2 = x0 * Math.cos(2 * Math.PI / 3) - y0 * Math.sin(2 * Math.PI / 3) + canvas.width / 2;
     const y2 = x0 * Math.sin(2 * Math.PI / 3) + y0 * Math.cos(2 * Math.PI / 3) + canvas.height / 2;
-    context.arc(x1, y1, r, 0, Math.PI * 2);
-    context.arc(x2, y2, r, 0, Math.PI * 2);
-    context.fill();
-    context.beginPath();
-    context.arc(x0 + canvas.width / 2, y0 + canvas.height / 2, r, 0, Math.PI * 2)
-    context.fill();
+    ctx.arc(x1, y1, r, 0, Math.PI * 2);
+    ctx.arc(x2, y2, r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(x0 + canvas.width / 2, y0 + canvas.height / 2, r, 0, Math.PI * 2)
+    ctx.fill();
   }
 };
 
